61d68961ed6ea0dd103df3dcd384ae7bcb3b6315
Purna Chandu Sanchi <107566783+Purna-Chandu-Sanchi@users.noreply.github.com>
2022-06-15 21:52:17 +0530

    Update streamlit_app.py
    working on the badge-2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas as pd
+
 streamlit.title("It's my First ever Streamlit application")
 streamlit.header("My SnowFlake Course")
 streamlit.text("It contains multiple Badges's")
@@ -6,3 +8,7 @@ streamlit.text("As of now I hv Completed the First Badge")
 streamlit.header("First Badge is 'Badge 1: Data Warehousing Workshop' ")
 streamlit.text(" Hands On Essentials - Data Warehouse")
 streamlit.text("It contains 11 Lesson's and Wrapping Up session for Badge")
+streamlit.header("Now in Badge two, We are on some excercise's ")
+streamlit.text("Fruit list")
+my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
