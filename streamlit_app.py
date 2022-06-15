c47ae5b7ebc212d51d8ae70b549758adc2a9c6e1
Purna Chandu Sanchi <107566783+Purna-Chandu-Sanchi@users.noreply.github.com>
2022-06-15 21:21:19 +0530

    Update streamlit_app.py
    Introduction

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 streamlit.title("It's my First ever Streamlit application")
- 
+streamlit.header("My SnowFlake Course")
+streamlit.text("It contains multiple Badges's")
+streamlit.text("As of now I hv Completed the First Badge")
