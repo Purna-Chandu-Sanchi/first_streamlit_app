df0766ce799526228cc644b4b1ea05864ff49355
Purna Chandu Sanchi <107566783+Purna-Chandu-Sanchi@users.noreply.github.com>
2022-06-17 14:40:13 +0530

    Update streamlit_app.py
    making dynamic of fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,9 @@ fruits_selected=streamlit.multiselect("Pick some fruits", list(my_fruit_list.ind
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/kiwi")
+fruit_choice=streamlit.text_input("What Fruit would you like information about?")
+streamlit.write("The user entered ",fruit_choice)
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 # streamlit.text(fruityvice_response.json())
 fruityvice_normalised=pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalised)
