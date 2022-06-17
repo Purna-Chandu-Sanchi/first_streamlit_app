839ce76e865b2b18fc348f370bb4a8562003b036
Purna Chandu Sanchi <107566783+Purna-Chandu-Sanchi@users.noreply.github.com>
2022-06-17 14:23:45 +0530

    Update streamlit_app.py
    using response library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas as pd
+import requests
 
 streamlit.title("It's my First ever Streamlit application")
 streamlit.header("My SnowFlake Course")
@@ -15,3 +16,5 @@ my_fruit_list=my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("Pick some fruits", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+fruityvice_response=responses.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
