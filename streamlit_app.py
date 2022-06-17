19785b97704a6086944ba980a474ac2fe9f0acc7
Purna Chandu Sanchi <107566783+Purna-Chandu-Sanchi@users.noreply.github.com>
2022-06-17 20:06:06 +0530

    connected to the snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,11 @@ streamlit.title("It's my First ever Streamlit application")
 streamlit.header("My SnowFlake Course")
 streamlit.text("It contains multiple Badges's")
 streamlit.text("As of now I hv Completed the First Badge")
+
 streamlit.header("First Badge is 'Badge 1: Data Warehousing Workshop' ")
 streamlit.text(" Hands On Essentials - Data Warehouse")
 streamlit.text("It contains 11 Lesson's and Wrapping Up session for Badge")
+
 streamlit.header("Now in Badge two, We are on some excercise's ")
 streamlit.text("Fruit list")
 my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -17,6 +19,7 @@ my_fruit_list=my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("Pick some fruits", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice=streamlit.text_input("What Fruit would you like information about?")
 streamlit.write("The user entered ",fruit_choice)
@@ -24,3 +27,14 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choic
 # streamlit.text(fruityvice_response.json())
 fruityvice_normalised=pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalised)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+
+
+
