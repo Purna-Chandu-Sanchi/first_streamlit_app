ae7e51605909ff516a8c918f8e583974a9d343a8
Purna Chandu Sanchi <107566783+Purna-Chandu-Sanchi@users.noreply.github.com>
2022-06-17 20:26:21 +0530

    adding fruit to fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-
+#choice of adding fruit
+add_my_fruit=streamlit.text_input("What Fruit would you like to add?")
+streamlit.write("Thanks for adding ",add_my_fruit)
